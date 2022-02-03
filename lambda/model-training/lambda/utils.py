255c09345dc6de98f8b82948205563a3a0998e32
CloudNative90 <98974746+CloudNative90@users.noreply.github.com>
2022-02-03 11:05:47 -0800

    Updated utils.py

--- lambda/model-training/lambda/utils.py
+++ lambda/model-training/lambda/utils.py
@@ -9,6 +9,13 @@ from joblib import dump, load
 
 def read_data(bucket: str, key: str) -> np.array:
     '''
+        Reads small CSV files from S3.
+        
+        args:
+            bucket: S3 bucket name
+            key: S3 path to the CSV file
+        returns:
+            np.array containing the data
     '''
     s3_csv = boto3.client("s3").get_object(Bucket=bucket, Key=key)
     csv_string = s3_csv["Body"].read().decode("utf-8")
@@ -18,6 +25,14 @@ def read_data(bucket: str, key: str) -> np.array:
 
 def save_model_to_s3(model, bucket: str, key: str) -> None:
     '''
+        Serializes a machine learning model and writes it to S3.
+        
+        args:
+            model: Scikit-learn model
+            bucket: S3 bucket name
+            key: S3 path where the serialized model will be written
+        returns:
+            None
     '''
     with tempfile.TemporaryFile() as fp:
         dump(model, fp)
